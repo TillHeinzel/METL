e8789843a84d0f6039eb628c5c67d7da0ce5c34d
Till Heinzel <tillheinzel@phys.au.dk>
2018-10-06 12:02:37 +0200

    remove redundant AUTO_Member file (is not in source)

--- test/AUTO_MEMBER.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "src/std17/remove_cvref.h"
-
-#define AUTO_MEMBER(membername, factoryFunction) \
-std17::remove_cvref_t<decltype(factoryFunction)> membername = factoryFunction
